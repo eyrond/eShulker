2c65765a29191b3d218fce445ef1429083c218c1
eyrond <admin@eyrond.dev>
2023-05-30 00:07:37 +0300

    Fix wrong hand prioritisation while opening shulker

--- src/main/kotlin/ShulkerInHandProcessor.kt
+++ src/main/kotlin/ShulkerInHandProcessor.kt
@@ -3,17 +3,28 @@ package dev.eyrond.shulker
 import dev.eyrond.paperkt.listener.KotlinListener
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.Action
+import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.ItemStack
 
 class ShulkerInHandProcessor : KotlinListener {
+
+    private val ItemStack.isShulker: Boolean
+        get() = type.name.endsWith("SHULKER_BOX")
 
     @EventHandler(ignoreCancelled = false)
     fun onShulkerRightClick(event: PlayerInteractEvent) {
         val isRightClick = event.action == Action.RIGHT_CLICK_AIR
         if (!isRightClick) return
-        val item = event.item ?: return
-        val isHoldingShulker = item.type.name.endsWith("SHULKER_BOX")
-        if (!isHoldingShulker) return
-        ShulkerInventory.open(event.player, item)
+        val player = event.player
+        val inventory = player.inventory
+        val shulker = if (inventory.itemInMainHand.isShulker) {
+            inventory.itemInMainHand
+        } else if (inventory.itemInOffHand.isShulker) {
+            inventory.itemInOffHand
+        } else return
+        val hasInventoryOpen = player.openInventory.type != InventoryType.CRAFTING
+        if (hasInventoryOpen) return
+        ShulkerInventory.open(event.player, shulker)
     }
 }
